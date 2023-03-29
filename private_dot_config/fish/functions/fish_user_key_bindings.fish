d85d3af9f128a73f6332e2793830bc6362f4c398
James Pulec <jpulec@gmail.com>
2023-03-29 13:12:44 -0700

    feat: use bat for fzf preview

--- private_dot_config/fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,4 @@
 function fish_user_key_bindings
-    bind \cp 'set -l file (fzf --preview="cat {}"); and nvim $file'
+    bind \cp 'set -l file (fzf --ansi --preview="bat --color=always --style=numbers,grid,changes {}"); and nvim $file'
     bind \cd delete-char
 end
